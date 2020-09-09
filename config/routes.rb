1f606c724cc9af807c3fbe944b4600a3ec1eb4e3
EC2 Default User <ec2-user@ip-172-31-32-173.ap-northeast-1.compute.internal>
2020-09-09 13:11:04 +0000

    resourcesは今回は使わないことにし、これまでのルーティングに戻す。

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
    devise_for :users
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'blogs#index'
-  resources :blogs
-  resources :users, only: [:show]
+  get 'blogs' => 'blogs#index'
+  get 'blogs/new' => 'blogs#new'
+  post 'blogs' => 'blogs#create'
+  delete 'blogs/:blog_id' => 'blogs#destroy'
+  patch 'blogs/:blog_id' => 'blogs#update'
+  get 'blogs/:blog_id/edit' => 'blogs#edit'
+  get 'blogs/:blog_id' => 'blogs#show'
+  get 'users/:id' => 'users#show'
   
   devise_scope :user do
     get 'users/sign_out' => 'devise/sessions#destroy'
